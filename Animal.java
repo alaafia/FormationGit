dbf880a963d2ab5a0f7f51c0a193d6fde58ef77c
alaafia <aichalaafia1@gmail.com>
2022-03-14 15:50:39 +0100

    only cats voice here

--- Animal.java
+++ Animal.java
@@ -6,16 +6,6 @@ public class Animal {
             return "I dont know";
     }
         
-    public static String getAnimalVoice(String animal) {
-        if (animal.equalsIgnoreCase("bird"))
-            return "Tweet";
-        else if (animal.equalsIgnoreCase("cat"))
-            return "Meowww !";
-        if (animal.equalsIgnoreCase("cow"))
-            return "moooooooh";
-        else  
-            return "I dont Know";
-    }
 
     public static void main(String[] args) {
         String firstArg = args[0];
